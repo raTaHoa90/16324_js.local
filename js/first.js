be5813e0511a30a7a02e7dc6e0692e31eab39e98
raTaHoa90 <160627104+raTaHoa90@users.noreply.github.com>
2026-01-16 22:07:02 +0300

    types, number

---
+++ js/first.js
@@ -0,0 +1,38 @@
+'strict';
+
+var num123_$ = 123, 
+    NUM123_$ = 'string value';
+let varEmpty;
+
+console.log(NUM123_$);
+
+let a = 5, b = 8;
+console.log(a + b); // 13
+console.log(a - b); // -3
+console.log(b / a); // 1.6
+console.log(a * b); // 40
+console.log(a % b); // 5
+console.log(a ** b);// 390625 
+console.log(-a);    // -5
+
+/*
+undefined
+Number
+String
+Boolean
+null
+Object
+    Object
+    Function
+    Array
+    Date
+    RegExp
+    Map
+    Set
+    WeakMap
+    WeakSet
+
+BigInt
+Symbol
+
+*/
