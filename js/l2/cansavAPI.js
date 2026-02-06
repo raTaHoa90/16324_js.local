192722a8696b9b2c3d265321015a76154f7be2a6
raTaHoa90 <160627104+raTaHoa90@users.noreply.github.com>
2026-02-06 20:47:04 +0300

    jquery, ajax, canvasAPI, audio/video API

---
+++ js/l2/cansavAPI.js
@@ -0,0 +1,55 @@
+var painting;
+
+class Painting {
+    #canvas;
+    #ctx;
+    #offset = 0;
+    #flagReverse = false;
+
+    constructor(canvasElement){
+        this.#canvas = canvasElement;
+        this.#ctx = canvasElement.getContext('2d');
+        canvasElement.width = 400;
+        canvasElement.height = 400;
+
+        this.draw();
+    }
+
+    draw(){
+        this.#ctx.strokeStyle = "black";
+        this.#ctx.lineWidth = 5;
+        this.#ctx.fillStyle = "red";
+
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+
+        this.#ctx.beginPath();
+            this.#ctx.moveTo(20 + this.#offset,18);
+            this.#ctx.lineTo(100 + this.#offset,100);
+            this.#ctx.lineTo(20 + this.#offset, 180);
+            this.#ctx.lineTo(20 + this.#offset,18);
+            this.#ctx.fill();
+            this.#ctx.stroke();
+            
+        if(this.#flagReverse)
+            this.#offset--;
+        else
+            this.#offset++;
+        
+        if(this.#offset == 0 || this.#offset == 25)
+            this.#flagReverse = !this.#flagReverse;
+
+        this.#ctx.closePath();
+    }
+}
+
+function AnimationFrame(){
+    painting.draw();
+    window.requestAnimationFrame(AnimationFrame);
+}
+
+
+//$(function(){
+document.addEventListener('DOMContentLoaded', function(){
+    painting = new Painting(document.getElementById('holst'));
+    window.requestAnimationFrame(AnimationFrame);
+});
